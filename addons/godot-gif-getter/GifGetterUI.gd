b440c4de6d4b84512bb64adb5b9cf7b46acbbbea
Tim Yuen <timothy.yuen22@gmail.com>
2021-03-06 11:17:35 -0500

    cleanup unused code, add new icon, remove gdscript gif library

--- addons/godot-gif-getter/GifGetterUI.gd
+++ addons/godot-gif-getter/GifGetterUI.gd
@@ -1,9 +1,5 @@
 extends CanvasLayer
 
-const GIF_EXPORTER = preload("res://addons/gdgifexporter/gifexporter.gd")
-const MEDIAN_CUT = preload("res://addons/gdgifexporter/quantization/median_cut.gd")
-const EUQ = preload("res://addons/gdgifexporter/quantization/enhanced_uniform_quantization.gd")
-
 const MAX_CONSOLE_MESSAGE_COUNT: int = 20
 
 onready var control: Control = $Control
@@ -19,10 +15,7 @@ onready var threads_line_edit: LineEdit = $Control/Options/VBoxContainer/Threads
 
 onready var console: VBoxContainer = $Control/Console/ScrollContainer/VBoxContainer
 
-onready var _viewport: Viewport = get_viewport()
-
-# Main exporter, coalesces all buffers into one and saves to file
-var _gif_exporter: GifExporter
+onready var _viewport_rid: RID = get_viewport().get_viewport_rid()
 
 # Determines if viewport texture data should be stored for processing
 var _should_capture: bool = false
@@ -42,30 +35,26 @@ var _max_frames: int = 20
 # Count frames stored
 var _current_frame: int = 1
 
+# Rendering quality for gifs from 1 - 30. 1 is highest quality but slow
 var _render_quality: int = 10
 
-# Main coalescing thread
-var _render_thread: Thread = Thread.new()
+# Background thread for capturing screenshots
+var _capture_thread: Thread = Thread.new()
 
 # Number of render threads
 var _max_threads: int = 4
-# Render thread storage
-var _render_threads: Array = []
 
-# Path to intended save location
-var _save_location: String = "user://result.gif"
+# Path to intended save location. Uses Rust's fs library instead of Godot's
+var _save_location: String = "result.gif"
 
+# Rust gif creation library
 var _gif_handler: Reference = load("res://addons/godot-gif-getter/GifHandler.gdns").new()
-
-var _capture_thread: Thread = Thread.new()
 
 ###############################################################################
 # Builtin functions                                                           #
 ###############################################################################
 
 func _ready() -> void:
-	_gif_exporter = GIF_EXPORTER.new(_viewport.size.x, _viewport.size.y)
-	
 	capture_now_button.connect("pressed", self, "_on_capture_now")
 	capture_in_five_seconds_button.connect("pressed", self, "_on_capture_in_five_seconds")
 	$Control/Options/VBoxContainer/SaveLocationContainer/Button.connect("pressed", self, "_on_select_path_button_pressed")
@@ -76,13 +65,6 @@ func _physics_process(_delta: float) -> void:
 			_capture_thread.start(self, "_capture_frames")
 
 func _exit_tree() -> void:
-	for i in _render_threads:
-		if i.is_active():
-			i.wait_to_finish()
-	
-	if _render_thread.is_active():
-		_render_thread.wait_to_finish()
-
 	if _capture_thread.is_active():
 		_capture_thread.wait_to_finish()
 
@@ -169,31 +151,10 @@ func _on_popup_hide() -> void:
 # Private functions                                                           #
 ###############################################################################
 
-func _godot_single_thread() -> void:
-	_render_thread.start(self, "_render_gif")
-
-func _godot_multi_thread() -> void:
-	var data: Array = []
-	for i in range(_max_threads):
-		_render_threads.append(Thread.new())
-		data.append([])
-	
-	# Divide up image data
-	# Will need to reconstruct this order later
-	var c: int = 0
-	for image in _images:
-		data[c].append(image)
-		if c < _max_threads - 1:
-			c += 1
-		else:
-			c = 0
-
-	for t in _render_threads.size():
-		_render_threads[t].start(self, "_write_frame_buffer_threaded", data[t])
-
-	_render_thread.start(self, "_render_gif_threaded", _render_threads)
-
 func _rust_multi_thread() -> void:
+	"""
+	Wrapper function for calling a Rust library to process and render a gif.
+	"""
 	var images_bytes: Array = []
 	for image in _images:
 		images_bytes.append(image.get_data())
@@ -203,23 +164,13 @@ func _rust_multi_thread() -> void:
 	_gif_handler.set_render_quality(_render_quality)
 	_gif_handler.write_frames(
 			images_bytes,
-			int(_viewport.size.x),
-			int(_viewport.size.y),
+			int(get_viewport().size.x),
+			int(get_viewport().size.y),
 			_max_threads,
 			_max_frames)
 
-# TODO getting buffers from rust doesn't work
-func _rust_godot_multi_thread() -> void:
-	var images_bytes: Array = []
-	for image in _images:
-		images_bytes.append(image.get_data())
-	var unsorted_buffers: Array = _gif_handler.get_buffers(images_bytes, int(_viewport.size.x), int(_viewport.size.y), _max_threads)
-	_render_thread.start(self, "_render_gif_from_buffers", unsorted_buffers)
-
 func _capture_frames(_x) -> void:
 	"""
-	_capture_frames
-	
 	Needs to be run on a background thread otherwise it blocks the main thread
 	when saving a viewport image.
 	
@@ -227,10 +178,9 @@ func _capture_frames(_x) -> void:
 	visible in it. Instead of solving that problem, just remove the last frame.
 	"""
 	while _should_capture:
-		print(_current_frame)
 		_frame_skip_counter += 1
 		if (_frame_skip_counter > _frame_skip and _current_frame <= _max_frames + 1):
-			var image: Image = _viewport.get_texture().get_data()
+			var image: Image = VisualServer.texture_get_data(VisualServer.viewport_get_texture(_viewport_rid))
 			image.convert(Image.FORMAT_RGBA8)
 			image.flip_y() # Images from the viewport are upside down
 			
@@ -252,130 +202,11 @@ func _capture_frames(_x) -> void:
 			
 			_capture_thread.call_deferred("wait_to_finish")
 
-func _render_gif(_x) -> void:
-	"""
-	_render_gif
-	
-	Single-threaded render and writing. Extremely slow.
-	An empty parameter is required when running on a thread.
-	"""
-	var file: File = File.new()
-	file.open(_save_location, File.WRITE)
-
-	for i in _images.size():
-		_log_message("Processing image %s of %s" % [i + 1, _images.size()])
-		
-		# TODO This has a memory leak
-#		var median_cut: MedianCut = MEDIAN_CUT.new()
-#		_gif_exporter.write_frame(_images[i], 1, median_cut)
-		
-		var euq = EUQ.new()
-		_gif_exporter.write_frame(_images[i], _gif_frame_delay, euq)
-
-	file.store_buffer(_gif_exporter.export_file_data())
-	file.close()
-	
-	_log_message("Gif saved")
-	
-	_render_thread.call_deferred("wait_to_finish")
-
-func _render_gif_threaded(data: Array) -> void:
-	"""
-	_render_gif_threaded
-	
-	data is an array containing the Threads used
-	"""
-	var buffer_data: Array = []
-	for t in data.size():
-		buffer_data.append(data[t].wait_to_finish())
-	
-	var reconstructed_buffer: PoolByteArray = PoolByteArray()
-	var c: int = 0
-	for i in range(_max_frames):
-		_log_message("Processing frame %s of %s" % [i + 1, _max_frames])
-		var buffer: PoolByteArray = buffer_data[c].pop_front()
-		if buffer:
-			reconstructed_buffer.append_array(buffer)
-		if c < data.size() - 1:
-			c += 1
-		else:
-			c = 0
-	
-	var file: File = File.new()
-	file.open(_save_location, File.WRITE)
-	
-	# Wait for the file to actually be open
-	while true:
-		if file.is_open():
-			break
-	
-	file.store_buffer(_gif_exporter.export_file_data_threaded(reconstructed_buffer))
-	file.close()
-	
-	_log_message("Gif saved")
-	
-	_render_thread.call_deferred("wait_to_finish")
-
-func _render_gif_from_buffers(data: Array) -> void:
-	var reconstructed_buffer: PoolByteArray = PoolByteArray()
-	var c: int = 0
-	for i in range(_max_frames):
-		_log_message("Processing frame %s of %s" % [i + 1, _max_frames])
-		var buffer: PoolByteArray = data[c].pop_front()
-		if buffer:
-			reconstructed_buffer.append_array(buffer)
-		if c < data.size() - 1:
-			c += 1
-		else:
-			c = 0
-	
-	var file: File = File.new()
-	file.open(_save_location, File.WRITE)
-	
-	# Wait for the file to actually be open
-	while true:
-		if file.is_open():
-			break
-	
-	file.store_buffer(_gif_exporter.export_file_data_threaded(reconstructed_buffer))
-	file.close()
-	
-	_log_message("Gif saved")
-	
-	_render_thread.call_deferred("wait_to_finish")
-
-func _write_frame_buffer_threaded(images: Array) -> Array:
-	"""
-	_write_frame_threaded
-	
-	images is an array containing the specific subset of images to convert
-	
-	return a regular Array instead of a PoolByteArray since the data is out of order
-	"""
-	var result: Array = []
-	var converted_images: Array = []
-	
-	var exporter: GifExporter = GIF_EXPORTER.new(_viewport.size.x, _viewport.size.y)
-	var euq = EUQ.new()
-	for i in images.size():
-		_log_message("Converting image %s of %s" % [i + 1, images.size()])
-		
-		var converted_image = exporter.convert_image(images[i], euq)
-		if converted_image.error == GifExporter.Error.OK:
-			# converted_images.append(converted_image)
-			result.append(exporter.write_frame_buffer_from_conv_image(converted_image.converted_image, _gif_frame_delay))
-		else:
-			_log_message("Error: %d" % converted_image.error, true)
-	
-	# converted_images
-	return result
-
 func _log_message(message: String, is_error: bool = false) -> void:
 	var label: Label = Label.new()
 	if is_error:
 		label.text += "[ERROR] "
 	label.text += message
-#	console.add_child(label)
 	console.call_deferred("add_child", label)
 	yield(label, "ready")
 	console.move_child(label, 0)
